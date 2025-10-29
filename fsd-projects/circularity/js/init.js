c6e257ac04caad6e9a9d74e06b36cf1feb5a2c06
kryniyahbrown5 <brownkryniyah1@gmail.com>
2025-10-29 13:22:23 +0000

    giggles

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -28,7 +28,7 @@ var circles = []
 
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
-            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            circle = draw.randomCircleInArea(canvas, true, true, "#FF00FF", 2);
             physikz.addRandomVelocity(circle, canvas, 5, 5);
             view.addChild(circle);
             circles.push(circle);
@@ -40,7 +40,7 @@ var circles = []
         
 
         // TODO 7 : Use a loop to create multiple circles
-        for (var i = 0; i < 100; i++){
+        for (var i = 0; i < 15000; i++){
          drawCircle();
         }
 
